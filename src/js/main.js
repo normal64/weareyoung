192a33fab3a602dea38e63361628161dbb152ff9
Denis Poplavskii <normal64@mail.ru>
2022-06-08 17:14:56 +0200

    close modal on icon close modal

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,18 @@ const onJustDontButtonClick = () => {
     const modal = document.querySelector(".modal-window")
     let classesToAdd = [ 'show',  ];
     modal.classList.add(...classesToAdd);
-    
     console.log(`modal`, modal);
-
 }
 const onJustDontButton = document.querySelector(".dont-button")
 onJustDontButton.onclick =() => onJustDontButtonClick();
 console.log(`onJustDontButton`, onJustDontButton);
+
+const closeModalButton = document.querySelector(".close.icon")
+console.log(`closeModalButton`, closeModalButton);
+closeModalButton.onclick = () => onCloseModalClick();
+const onCloseModalClick = () => {
+    console.log("close modal");
+    const modal = document.querySelector(".modal-window")
+    modal.classList.remove('show')
+
+}
